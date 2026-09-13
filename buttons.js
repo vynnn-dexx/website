2e532dc13fe0ab8de039f262273db64e17d30dca
vynnn-dexx <humanerorivy@gmail.com>
2026-09-14 04:31:01 +0700

    Update buttons.js

--- buttons.js
+++ buttons.js
@@ -33,17 +33,4 @@ const scrapeButtonsData = [
     previewUrl: "#",
     targetUrl: "quran.html"
   }
-  
-,
-  {
-    id: "anime",
-    title: "straming snime",
-    description: "untuk menonton anime secara realtime",
-    author: "ARCVN",
-    method: "PUT",
-    category: "anime-stram",
-    imageUrl: "https://files.catbox.moe/diu9bo.png",
-    previewUrl: "#",
-    targetUrl: "animek.html"
-  }
 ];
